f68faa90cfdac27d7c19c07d6fe35ccb3ae01715
DaliaZH <121820334+AhmedZahran15@users.noreply.github.com>
2025-01-29 00:25:26 +0200

    refactor video player functionality

--- script.js
+++ script.js
@@ -9,28 +9,12 @@ const volumeBar = document.querySelector("#volumeBar");
 const fullScreenBtn = document.querySelector("#fullScreenBtn");
 const videoControls = document.querySelector(".videoControls");
 const videoTitle = document.querySelector("#videoTitle");
-// const tracks = document.querySelectorAll("track");
 const subtitleDiv = document.querySelector(".videoSubtitles");
-
-videos.forEach((video, index) => {
-  const button = document.createElement("button");
-  button.textContent = `Video ${index + 2}`;
-  document.querySelector(".videos").appendChild(button);
-  button.addEventListener("click", () => {
-    myVideo.src = video.sources[0];
-    videoTitle.textContent = video.title;
-    videosButtons.forEach((video) => video.classList.remove("selected"));
-    button.classList.add("selected");
-  });
-});
-const videosButtons = document.querySelectorAll("aside button");
-videosButtons[0].addEventListener("click", () => {
-  myVideo.src = "./Videos/BiteMarksVideo.mp4";
-  videoTitle.textContent = "Bite Marks - League of Legends";
-  videosButtons.forEach((video) => video.classList.remove("selected"));
-  videosButtons[0].classList.add("selected");
-});
-
+const duration = document.querySelector("#duration");
+const currentTime = document.querySelector("#currentTime");
+const progress = document.querySelector(".progress");
+const progressBar = document.querySelector(".progressBar");
+const videosButtons = renderVideos(videos);
 
 myVideo.parentElement.addEventListener("click", handlePlayPause);
 playPauseBtn.addEventListener("click", handlePlayPause);
@@ -41,10 +25,8 @@ fullScreenBtn.addEventListener("click", handleFullScreenChange);
 myVideo.addEventListener("dblclick", handleFullScreenChange);
 volumeBar.addEventListener("input", handleVolumeChange);
 myVideo.addEventListener("ended", handleNextVideo);
-/* myVideo.addEventListener("timeupdate", () => {
-  const percentage = (myVideo.currentTime / myVideo.duration) * 100;
-  document.querySelector("#progressBar").style.width = `${percentage}%`;
-}); */
+myVideo.addEventListener("timeupdate", updateTime);
+progress.addEventListener("click", handleProgressClick);
 
 videoControls.addEventListener("click", (e) => {
   e.stopPropagation();
@@ -75,6 +57,53 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
+myVideo.parentElement.addEventListener("mousemove", () => {
+  if (!myVideo.paused) {
+    videoControls.style.opacity = 1;
+    videoTitle.style.opacity = 1;
+  }
+  setTimeout(() => {
+    if (myVideo.paused) return;
+    videoControls.style.opacity = 0;
+    videoTitle.style.opacity = 0;
+  }, 3000);
+});
+
+function renderVideos(videos) {
+  videos.forEach((video, index) => {
+    const button = document.createElement("button");
+    button.textContent = `Video ${index + 1}`;
+    document.querySelector(".videos").appendChild(button);
+  });
+  const buttons = document.querySelectorAll(".videos button");
+  buttons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      myVideo.src = videos[index].sources[0];
+      myVideo.poster = videos[index].thumb;
+      videoTitle.textContent = videos[index].title;
+      buttons.forEach((video) => video.classList.remove("selected"));
+      button.classList.add("selected");
+      playPauseBtn.children[0].src = "./images/icons/play.svg";
+      videoControls.style.opacity = 1;
+      videoTitle.style.opacity = 1;
+    });
+  });
+  buttons[0].click();
+  return buttons;
+}
+function updateTime() {
+  currentTime.textContent = formatTime(myVideo.currentTime);
+  duration.textContent = formatTime(myVideo.duration);
+  progressBar.style.width = `${
+    (myVideo.currentTime / myVideo.duration) * 100
+  }%`;
+}
+
+function handleProgressClick(e) {
+  const rect = progress.getBoundingClientRect();
+  const pos = (e.clientX - rect.left) / progress.offsetWidth;
+  myVideo.currentTime = pos * myVideo.duration;
+}
 function handleVolumeChange() {
   myVideo.volume = +volumeBar.value;
   if (myVideo.volume) {
@@ -114,7 +143,6 @@ function handleFullScreenChange() {
 }
 function handleNextVideo() {
   previousBtn.style.display = "block";
-  playPauseBtn.children[0].src = "./images/icons/play.svg";
   if (!videosButtons[videosButtons.length - 1].classList.contains("selected")) {
     videosButtons[
       Array.from(videosButtons).findIndex((video) =>
@@ -127,7 +155,6 @@ function handleNextVideo() {
 }
 function handlePreviousVideo() {
   nextBtn.style.display = "block";
-  playPauseBtn.children[0].src = "./images/icons/play.svg";
   if (!videosButtons[0].classList.contains("selected")) {
     videosButtons[
       Array.from(videosButtons).findIndex((video) =>
@@ -148,3 +175,16 @@ track.addEventListener("cuechange", () => {
     subtitleDiv.textContent = "";
   }
 });
+
+function formatTime(time) {
+  if (!isNaN(time)) {
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = Math.floor(time % 60);
+    return `${hours > 9 ? hours.toString().padStart(2, "0") + ":" : ""}${
+      minutes > 9 ? minutes.toString().padStart(2, "0") : minutes
+    }:${seconds.toString().padStart(2, "0")}`;
+  } else {
+    return "00:00";
+  }
+}
